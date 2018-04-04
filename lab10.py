02880bd6b9292620c9fba28a4173bb584f600a17
Kirill <kirill_kurguzkin@mail.ru>
2018-04-04 10:47:02 +0400

    ver 2.0

--- lab10.py
+++ lab10.py
@@ -1,16 +1,18 @@
 import random
 #1. Составить процедуру (функцию), определяющую, в каком из данных двух чисел больше цифр
-def first(temp1,temp2):
-    while temp1 > 1:
-        temp1 = temp1//10
-        temp2 = temp2//10
-    if(temp1>temp2):
-        return "В первом больше"
-    elif(temp1<temp2):
-        return "Во втором больше"
-    elif(temp1==temp2):
-        return "Одинаково"
-print (first(12345,12))
+def countDigit(number):
+    count = 0
+    if(number == 0):
+        return count
+    else:
+        while number > 0:
+            number = number//10
+            count += 1
+    return count
+if(countDigit(0) > countDigit(12)):
+    print ("В первом больше")
+else:
+    print ("Во втором больше")
 #2. Вывести на печать матрицу с наименьшей нормой.
 def fill_matrix(x,y,n):
     matrix = [[random.randint(x,y) for i in range(n)] for i in range(n)]
@@ -26,16 +28,7 @@ A = fill_matrix(-100,100,3)
 B = fill_matrix(-100,100,3)
 C = fill_matrix(-100,100,3)
 max1,max2,max3 = max_abs(A),max_abs(B),max_abs(C)
-if(max1 <= max2):
-    if(max1 <= max3):
-        print(max1)
-    else:
-        print(max3)
-else:
-    if(max2 <= max3):
-        print(max2)
-    else:
-        print(max3)
+print("Матрица с наименьшей нормой: ",max(max1,max2,max3))
 #3. Описать функцию С(m,n), для вычисления биномиального коэффициента
 def binom(m, n):
     if (0 < m < n):
